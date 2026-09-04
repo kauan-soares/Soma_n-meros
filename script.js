d58d1f6aafc1283ed118eb584f7c7adf1b8eec00
kauan-soares <kauansmvlogs@gmail.com>
2026-09-04 12:08:46 -0300

    2.0

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-
 function somaNumeros(){
-
-    var tn1 = window.document.getElementById("num1").value
-    var tn2 = window.document.getElementById("num2").value
+    
+    var num1 = window.document.getElementById("num1").value
+    var num2 = window.document.getElementById("num2").value
     var resultado = window.document.getElementById("txtresultado")
 
-    tn1 = Number(tn1)
-    tn2 = Number(tn2)
-    var soma = tn1 + tn2
+    num1 = Number(num1)
+    num2 = Number(num2)
+    var soma = num1 + num2
 
     resultado.value = soma
 }
