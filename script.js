6cceebde3ac9d14a829efd71a62c6514474351db
kauan-soares <kauansmvlogs@gmail.com>
2026-09-04 22:15:26 -0300

    Calcular idade

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function somaNumeros(){
-    
-    var num1 = window.document.getElementById("num1").value
-    var num2 = window.document.getElementById("num2").value
-    var resultado = window.document.getElementById("txtresultado")
+function calcularIdade() {
+    var dataNascimento = window.document.getElementById("dataNascimento").value
+    var resultado = window.document.getElementById("resultado") 
 
-    num1 = Number(num1)
-    num2 = Number(num2)
-    var soma = num1 + num2
+    var dtn = new Date(dataNascimento)
+    var hoje = new Date()
+    var idade = Math.floor(
+            Math.ceil(
+                Math.abs( dtn.getTime() - hoje.getTime())
+                / (1000 * 3600 * 24)
+                     ) /365.25
+            )
 
-    resultado.value = soma
+            resultado.value = "A idade é " + idade + " ano(s)"
+
 }
